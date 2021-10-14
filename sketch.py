4dd6087f858714f294d71e84874d4e541f8b4581
Davcg5 <davcg5@hotmail.com>
2021-10-13 21:27:55 -0500

    split function getKeys and splitters in functions

--- sketch.py
+++ sketch.py
@@ -1,42 +1,52 @@
+CHARACTERSOFDAY = 2
+
+
 def convertToRange(stringRange):
   stringList = stringRange.split("-")
   numericalRange = tuple(map(lambda x: convertStringDateToDecimal(x), stringList))
   return numericalRange
+def fillDict(name, schedule):
+  for day in schedule: 
+    
+    dayName, time = separateDayNameAndTime(day)
+    dicti[dayName] = {}
+    dicti[dayName][time] = [name]
 
 def convertStringDateToDecimal(string): 
   h,m = string.split(":")
   return float(int(h)+int(m)/60)
+def separateDayNameAndTime(day): 
+  return day[:CHARACTERSOFDAY], convertToRange(day[CHARACTERSOFDAY:])
+def separateNameAndSchedule(chain):
+  name, schedule = chain.split("=")
+  return name, schedule 
 
-def getKeys(hours):
-  hours = hours.split("=")
-  name = hours[0]
-  hours=hours[1]
-  print("name", name)
-  for h in hours.split(","):
-    day = h[:2]
-    k = convertToRange(h[2:])
-    if day not in dicti.keys(): 
-      dicti[day] = {}      
-      if k not in dicti[day].keys():
-        dicti[day][k] = [name]
+def checkCrosses(name, schedule):
+
+  for day in schedule: 
+      dayName, time = separateDayNameAndTime(day)
+
+      if dayName not in dicti.keys(): 
+          fillDict(name, schedule)
       else: 
-        print("aca name con ", dicti[day][k])
-    else: 
-      if k not in dicti[day].keys():
-        dicti[day][k] = [name]
-      else: 
-        for al in dicti[day][k]: 
-          print(f"encuentra {al} con {name}")
-        dicti[day][k] = dicti[day][k] +[name]
-        
+        if time not in dicti[dayName].keys():
+          dicti[dayName][time] = [name]
+        else: 
+          for al in dicti[dayName][time]: 
+            print(f"esta {al} con {name}")
+            dicti[dayName][time] = list(set(dicti[dayName][time] +[name]))
 
 personal = ['RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00', 'ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00', 'ANDRES=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00']
 
 dicti={}
-fdict={}
 for p in personal: 
-  a = p.split("=")
-  b = getKeys(p)
-
-
+  name, schedule = separateNameAndSchedule(p)
+  print("llega", name)
+  schedule = schedule.split(",")
+  if (len(dicti.keys() )==0): 
+    fillDict(name,schedule)
+  else: 
+    checkCrosses(name, schedule)
+  
+  
 print(dicti)
