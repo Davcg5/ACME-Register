eb4e89d17b3762180029eb024c7caa9bd0210e15
Davcg5 <davcg5@hotmail.com>
2021-10-16 19:38:37 -0500

    class File

---
+++ File.py
@@ -0,0 +1,34 @@
+from Exceptions import EmptyFileException, MinLenException, IncorrectFormat
+class File(): 
+    def __init__(self, fileName): 
+        self.errors =[]
+
+        if self.validateExtension(fileName): 
+            self.registers = open(fileName)
+            self.registers = self.registers.readlines()
+            self.cleanFile()
+            self.checkNumberOfLines()
+        else: 
+            raise IncorrectFormat()
+    def cleanFile(self): 
+        self.registers = list(line for line in (l.strip() for l in self.registers) if line)
+    
+    def validateExtension(self, fileName): 
+        extension = fileName.split(".")[-1]
+        if extension == "txt": 
+            return True
+        else: 
+            return False
+        
+
+    def checkNumberOfLines(self):
+        
+        if len(self.registers) ==0:
+            raise  EmptyFileException
+        elif len(self.registers) <3 : 
+            raise MinLenException
+  
+    def addError(self, error): 
+        self.errors.append(error)
+        
+    # def retrieveFilterFile():
